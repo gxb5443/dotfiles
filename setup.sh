34a4cebb48f7ca8e8f918ed5ab0a90a785378fc5
Gianfranco Biondi <gianfranco.biondi@jet.com>
2019-06-13 12:39:09 -0400

    cleans up config for jet

--- setup.sh
+++ setup.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 BASE_DIRECTORY="$HOME/Documents"
 DIRECTORY_LOCATION=$BASE_DIRECTORY/dotfiles
 
-#if [[ "$OSTYPE" == "darwin"* ]]; then
-#  echo "Yup, mac alright..."
-#  if ! command -v foo 2>/dev/null; then
-#    echo "Homebrew not found. Installing now..."
-#    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-#  fi
-#  brew bundle
-#fi
+ln -fs $DIRECTORY_LOCATION/.hammerspoon $HOME/.hammerspoon
 
-ln -fs $DIRECTORY_LOCATION/.hammerspoon/init.lua $HOME/.hammerspoon/init.lua
-
-ln -fs $DIRECTORY_LOCATION/.inputrc $HOME/.inputrc
-ln -fs $DIRECTORY_LOCATION/.tmux.conf $HOME/.tmux.conf
+#ln -fs $DIRECTORY_LOCATION/.inputrc $HOME/.inputrc
+#ln -fs $DIRECTORY_LOCATION/.tmux.conf $HOME/.tmux.conf
 
 ln -fs $DIRECTORY_LOCATION/config/nvim/init.vim $HOME/.config/nvim/init.vim
 
