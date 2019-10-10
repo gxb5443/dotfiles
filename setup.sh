94090a5e34e6648df789850dc4ee093f9b28dad7
Gian Biondi <biondimeister@gmail.com>
2019-10-10 11:18:43 -0400

    Revert "cleans up config for jet"
    This reverts commit 34a4cebb48f7ca8e8f918ed5ab0a90a785378fc5.

--- setup.sh
+++ setup.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 BASE_DIRECTORY="$HOME/Documents"
 DIRECTORY_LOCATION=$BASE_DIRECTORY/dotfiles
 
-ln -fs $DIRECTORY_LOCATION/.hammerspoon $HOME/.hammerspoon
+#if [[ "$OSTYPE" == "darwin"* ]]; then
+#  echo "Yup, mac alright..."
+#  if ! command -v foo 2>/dev/null; then
+#    echo "Homebrew not found. Installing now..."
+#    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#  fi
+#  brew bundle
+#fi
 
-#ln -fs $DIRECTORY_LOCATION/.inputrc $HOME/.inputrc
-#ln -fs $DIRECTORY_LOCATION/.tmux.conf $HOME/.tmux.conf
+ln -fs $DIRECTORY_LOCATION/.hammerspoon/init.lua $HOME/.hammerspoon/init.lua
+
+ln -fs $DIRECTORY_LOCATION/.inputrc $HOME/.inputrc
+ln -fs $DIRECTORY_LOCATION/.tmux.conf $HOME/.tmux.conf
 
 ln -fs $DIRECTORY_LOCATION/config/nvim/init.vim $HOME/.config/nvim/init.vim
 
